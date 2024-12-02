50120d96789f01c3d404a608109cfbfee21e0d4a
Zainab4523 <202004523@stu.uob.edu.bh>
2024-12-02 13:23:39 +0300

    delete all

--- db/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $conn = mysqli_connect('localhost' , 'root' , '' , 'Itcs333-project');
-    if(!$conn){
-        die('error'.myaqli_connect_error());
-    }
-?>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include('db/connection.php');
-session_unset();
-session_destroy();
-header('location:index.php');
-?>
