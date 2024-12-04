897039d1193165638f7a9283b7b7118ff18ee96d
Zainab4523 <202004523@stu.uob.edu.bh>
2024-12-04 21:17:33 +0300

    style

--- login.php
+++ login.php
@@ -29,16 +29,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="pico-main/css/pico.min.css">
     <title>User Login</title>
 </head>
 <body>
-
-    <h1>Login</h1>
+<h1   style="text-align: center;" >Login</h1>
+<fieldset class="grid">
     <form action="login.php" method="POST">
         <input type="email" name="email" placeholder="Email" required>
         <input type="password" name="password" placeholder="Password" required>
         <button type="submit">Login</button>
     </form>
-
+</fieldset>
 </body>
 </html>
