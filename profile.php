d868cadbbeb128f8013d2f720daa66f5f7f513db
Zainab4523 <202004523@stu.uob.edu.bh>
2024-12-04 22:29:13 +0300

    update profile

--- profile.php
+++ profile.php
@@ -12,7 +12,6 @@ $sql = "SELECT * FROM users WHERE email='$email'";
 $result = mysqli_query($conn, $sql);
 $user = mysqli_fetch_assoc($result);
 
-// Set a default profile picture if none exists
 $profile_picture = !empty($user['profile_picture']) ? "uploads/" . $user['profile_picture'] : "uploads/no_picture.png"; // Path to your no-picture icon
 ?>
 
@@ -21,13 +20,41 @@ $profile_picture = !empty($user['profile_picture']) ? "uploads/" . $user['profil
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="pico-main/css/pico.min.css">
     <title>User Profile</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center; 
+            justify-content: center; 
+        }
+        h1 {
+          --pico-font-family: Pacifico, cursive;
+          --pico-font-weight: 400;
+          --pico-typography-spacing-vertical: 0.5rem;
+        }
+
+        img {
+            border-radius: 50%;
+            margin-bottom: 1rem;
+        }
+
+       
+    fieldset {
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 2px 10px white;
+            width: 600px;            
+        }
+
+    </style>
 </head>
 <body>
-<div class="container">
+
     <h1>User Profile</h1>
     <img src="<?php echo $profile_picture; ?>" alt="Profile Picture" width="150" height="150">
+    <fieldset class="grid">
     <form action="update_profile.php" method="POST" enctype="multipart/form-data">
         <input type="text" name="username" value="<?php echo $user['username']; ?>" required>
         <input type="email" name="email" value="<?php echo $user['email']; ?>" readonly>
@@ -38,8 +65,11 @@ $profile_picture = !empty($user['profile_picture']) ? "uploads/" . $user['profil
         </select>
         <input type="file" name="profile_picture">
         <button type="submit">Update Profile</button>
+        <div role="button" tabindex="0" style="background-color:white;">
+        <a href="logout.php" >Logout</a>
+        </div>
     </form>
-    <a href="logout.php">Logout</a>
-</div>
+</fieldset>
+
 </body>
 </html>
