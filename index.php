39bc0dda0a2ae63e7c197b709690d8afdc612265
Zainab4523 <202004523@stu.uob.edu.bh>
2024-12-04 21:44:36 +0300

    css

--- index.php
+++ index.php
@@ -5,25 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="pico-main/css/pico.min.css">
     <title>Home - IT College Room Booking System</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; 
+          }
+        :root {
+          --pico-border-radius: 2rem;
+          --pico-typography-spacing-vertical: 1.5rem;
+          --pico-form-element-spacing-vertical: 1rem;
+          --pico-form-element-spacing-horizontal: 1.25rem;
+        }
+        h1 {
+          --pico-font-family: Pacifico, cursive;
+          --pico-font-weight: 400;
+          --pico-typography-spacing-vertical: 0.5rem;
+        }
+
+</style>
 </head>
 <body>
     <div class="container">
-    <style>
-  :root {
-    --pico-border-radius: 2rem;
-    --pico-typography-spacing-vertical: 1.5rem;
-    --pico-form-element-spacing-vertical: 1rem;
-    --pico-form-element-spacing-horizontal: 1.25rem;
-  }
-  h1 {
-    --pico-font-family: Pacifico, cursive;
-    --pico-font-weight: 400;
-    --pico-typography-spacing-vertical: 0.5rem;
-  }
-  #button {
-    --pico-font-weight: 700;
-  }
-</style>
         <h1>Welcome to the IT College Room Booking System</h1>
         <p>Your one-stop solution for managing room bookings at IT College.<br>
          Easily register, log in, and manage your bookings with our user-friendly interface.</p>
